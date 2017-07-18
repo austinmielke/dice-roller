4d4ace664b5fd2ca99e31d6a7e8e05e5227ea25d
Austin Mielke <amielkedev@gmail.com>
2017-07-17 23:48:35 -0500

    Added files and updated ReadMe

---
+++ roll.js
@@ -0,0 +1,29 @@
+//Variables Defined
+var rollDisplay = document.querySelector("#rollDisplay");
+var rollButton = document.querySelector("#rollButton");
+var numInput = document.querySelector("#numInput");
+var maxRoll = 6;
+var rollDisplayValue;
+
+//Functions Defined
+function randomRoll(){
+  return Math.floor(Math.random() * maxRoll +1);
+}
+
+numInput.addEventListener("change", function(){
+  maxRoll = Number(this.value);
+});
+
+rollButton.addEventListener("click", function(){
+  rollDisplay.classList.remove("criticalFailure", "criticalHit");
+  rollDisplayValue = randomRoll();
+  if (rollDisplayValue === 1){
+    rollDisplayValue += " - Critical Failure!";
+    rollDisplay.classList.add("criticalFailure");
+  }
+  if (rollDisplayValue === maxRoll){
+    rollDisplayValue += " - Critical Hit!";
+    rollDisplay.classList.add("criticalHit");
+  }
+  rollDisplay.textContent = rollDisplayValue;
+});
